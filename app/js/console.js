1251edd29dfa9abe63ed23086ca572c03eb35d3f
Boris Shabanov <b.shabanov@cti.ru>
2013-03-17 17:59:40 +0400

    залил проект

---
+++ app/js/console.js
@@ -0,0 +1,78 @@
+top.params = {
+	'server' : {
+		'port' : 7760
+	}
+};
+
+$(document).ready(function() {
+	init_server();
+
+	$('.current_request input').keypress(function(k) {
+	  if (k.charCode == 13) {
+	  	send_msg($('.current_request input').val());
+	  }
+	});
+});
+
+function print_request(text) {
+	var request = document.createElement('div');
+	request.innerText = ' ' + text;
+	request.className = 'request';
+
+	$('.console').get(0).appendChild(request);
+	
+	$("html,body").animate({"scrollTop":$("html,body").height()},100);
+}
+
+function print_answer(text,type) {
+	if (!type) {
+		var type = 'info';
+	}
+	var answer = document.createElement('div');
+	answer.innerText = ' ' + text;
+	answer.className = 'answer ' + type;
+
+	$('.console').get(0).appendChild(answer);
+	
+	$("html,body").animate({"scrollTop":$("html,body").height()},100);
+}
+
+function send_msg(code) {
+	print_request(code);
+	$('.current_request input').val('');
+}
+
+function init_server() {
+	var http = require('http');
+	
+	http.createServer(function (req, res) {
+		var url_parts = require('url').parse(req.url, true);
+
+		if (url_parts.pathname == '/rd.js') {
+
+			var fs = require('fs');
+			fs.readFile('./js/rd.js', 'utf8', function (err,data) {
+				res.writeHead(200, {'Content-Type': 'text/javascript'});
+				res.end(data);
+			});
+
+		} else {
+
+			var get_params = url_parts.query;
+
+			if (get_params.t && get_params.m) {
+				print_answer(get_params.m, get_params.t);	
+			}
+
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('');
+		}
+
+	}).listen(params.server.port, '127.0.0.1');
+	
+	$('.console').html('<strong>НАЧАЛО</strong>'
+		+ '<p>Для запуска скрипта добавьте на своей странице в BODY:<pre><code>&lt;script src="http://127.0.0.1:' + params.server.port + '/rd.js" type="text/javascript"&gt;&lt;/script&gt;</code></pre></p>'
+		+ '<p>Что бы отправить сообщение в консоль выполните в своем коде одну из функций:<pre><code>rd.log(\'MESSAGE\');\nrd.info(\'MESSAGE\');\nrd.error(\'MESSAGE\');\nrd.warn(\'MESSAGE\');</code></pre></p>'
+		+ '<p><strong>TODO</strong></p><ul><li>Отправка сообщений из консоли.</li></ul>'
+	);
+}
---
+++ app/js/rd.js
@@ -0,0 +1,63 @@
+Remote_debugger = function () {
+  this.options = {
+    'use_img_for_send':false,
+    'server':'127.0.0.1:7760'
+  };
+
+  this.getXmlHttp = function (){
+  var xmlhttp;
+  try {
+  xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+  } catch (e) {
+  try {
+  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  } catch (E) {
+  xmlhttp = false;
+  }
+  }
+  if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+  xmlhttp = new XMLHttpRequest();
+  }
+  return xmlhttp;
+  };
+
+  this.send = function(m,t) {
+    var uri = 'http://' + this.options.server + '/1.txt?t=' + t + '&m=' + m;
+    if (rd.options.use_img_for_send) {
+      var img = document.getElementById('remote_debugger_img');
+      img.src = uri;
+    } else {
+      var xmlhttp = this.getXmlHttp()
+      xmlhttp.open('GET', uri, false);
+      xmlhttp.send(null);
+      if(xmlhttp.status == 200) {
+        alert(xmlhttp.responseText);
+      }
+    }
+  };
+
+  this.log = function(t){
+    this.send(t,'log');
+  }
+
+  this.info = function(t){
+    this.send(t,'info');
+  }
+
+  this.error = function(t){
+    this.send(t,'error');
+  }
+
+  this.warn = function(t){
+    this.send(t,'warn');
+  }
+  return this;
+};
+
+var rd = new Remote_debugger();
+
+if (rd.options.use_img_for_send) {
+document.writeln('<img id="remote_debugger_img" style="display:none;">');
+}
+
+rd.info('Соединение c JS установлено');
