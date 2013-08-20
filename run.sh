3bd10b4daa42ab5d093c16dbc796c34eea81b2e0
Boris Shabanov <b.shabanov@cti.ru>
2013-08-20 14:54:28 +0400

    add support mac os

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-./bin/nw  ./app
+
+
+PLATFORM=`uname -s`
+
+if [ $PLATFORM = "Linux" ]; then
+	./bin_linux/nw  ./app
+elif [ $PLATFORM = "FreeBSD" ]; then
+  echo "NO SUPPORT"
+elif [ $PLATFORM = "Darwin" ]; then
+	./bin_mac/node-webkit.app/Contents/MacOS/node-webkit  ./app
+else
+  echo "The $PLATFORM platform is not currently supported."
+  exit 2
+fi
