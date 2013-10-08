cc07b069bd48ae324a88e70d78a95351fd491314
Boris Shabanov <b.shabanov@cti.ru>
2013-10-08 12:08:02 +0400

    add icon

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ top.config = {
 	'server' : {
 		'ip' : '89.175.77.60',
 		'port' : '7760',
-		'use_img_for_send' : 'true',
+		'use_img_for_send' : 'false',
 		'use_rnd_in_img_for_send' : 'false',
-		'use_comet' : 'false',
+		'use_comet' : 'true',
 		'xmlhttp_object' : 'new XMLHttpRequest()' // IE: ActiveXObject("Msxml2.XMLHTTP") / Other browser: new XMLHttpRequest()
 
 	}
